cebbf75c681ef51ffda5ac619badcdf3fed04b6d
JohnySapo <B00139596@mytudublin.ie>
2024-02-17 13:11:06 +0000

    1st Commit Branch initDBNewUser. Connection to MySQL & Form Create New User and store to db.

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+    $host = "localhost";
+    $username = "root";
+    $password = "Hakuna21@";
+    $dbname = "test";
+    $dsn = "mysql:host=$host;dbname=$dbname";
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+?>
---
+++ install.php
@@ -0,0 +1,20 @@
+<?php 
+    require "config.php";
+
+    // $connection = new PDO("mysql:host=$host", "$username", "$password", $options);
+
+    // $sql = file_get_contents("data/init.sql");
+    // $connection -> exec ($sql);
+    // echo "DB Setup";
+
+
+    try {
+        $connection = new PDO("mysql:host=$host", "$username", "$password", $options);
+        $sql = file_get_contents("data/init.sql");
+        $connection -> exec ($sql);
+
+        echo "Database and table users created successfully.";
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    } 
+?>
